04f7c885f2793c96954f625cb5cdf9200bcd31f7
fredinye <fredy.mamani.canahua@gmail.com>
2023-03-30 21:07:30 -0500

    jdango04

---
+++ .history/veterinaria/gestion/views_20230330194956.py
@@ -0,0 +1,9 @@
+from rest_framework.views import APIView
+from rest_framework.request import Request
+
+class RegistroUsuario(APIView):
+
+    def post(self, request: Request):
+        pass
+
+# Create your views here.
---
+++ .history/veterinaria/gestion/views_20230330201922.py
@@ -0,0 +1,27 @@
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from .serializers import RegistroUsuarioSerializer
+from .models import Usuario
+from rest_framework.response import Response
+from rest_framework import status
+
+class RegistroUsuario(APIView):
+
+    def post(self, request: Request):
+        serializador = RegistroUsuarioSerializer(data = request.data)
+        if serializador.is_valid():
+            password = serializador.validated_data.get('password')
+            nuevo_usuario = Usuario(**serializador.validated_data)
+            # generar el hash de la password
+            nuevo_usuario.set_password(password)
+
+            return Response(data={
+                'message': 'Usuario creadp exitosamente'
+            },status=status.HTTP_201_CREATED)
+        else:
+            return Response(data= {
+                'message': 'Error al crear el usuario',
+                'content': serializador.errors
+            },status=status.HTTP_400_CREATED)
+
+# Create your views here.
